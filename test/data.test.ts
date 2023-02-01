3dc52d5ce13f51d6caa33a36e5892ea37a8258d2
YongGit <1013588891@qq.com>
2023-02-01 09:56:29 +0800

    types: fix deep-clone type

--- test/data.test.ts
+++ test/data.test.ts
@@ -31,8 +31,6 @@ describe('js-utils-dataFns', () => {
     expect(dataFns.deepClone(target).obj).not.toBe(target.obj)
     expect(dataFns.deepClone(target)).toEqual(target)
     expect(target).toBe(target)
-    // @ts-ignore
-    expect(() => dataFns.deepClone('')).toThrow('Target must be object')
     expect(() => dataFns.deepClone(target)).not.toThrow('Target must be object')
   })
 })
