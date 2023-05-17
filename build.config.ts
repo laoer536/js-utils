f66fab3bca67e135863e5668eddbff0a11f9a216
laoer536 <1547698569@qq.com>
2023-05-18 00:33:12 +0800

    style: build配置简化

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,6 @@
 import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
-  name: 'js-utils-pro',
-  entries: ['./src/index', './src/is', './src/data', './src/string', './src/dom'],
-  declaration: true,
-  clean: true,
-  failOnWarn: false,
   rollup: {
-    emitCJS: true,
     esbuild: {
       minify: true,
     },
